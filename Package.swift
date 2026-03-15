079cbe362a000bd636a6cbead4d6acbc7b82a375
Mathieu Beaudin <macthieu@me.com>
2026-03-15 18:01:56 -0400

    build(deps): pin OrchivisteKit to v0.2.0 for canonical contracts

--- Package.swift
+++ Package.swift
@@ -8,18 +8,36 @@ let package = Package(
     ],
     products: [
         .library(name: "MuniMetadonneesCore", targets: ["MuniMetadonneesCore"]),
+        .library(name: "MuniMetadonneesInterop", targets: ["MuniMetadonneesInterop"]),
         .executable(name: "muni-metadonnees-cli", targets: ["MuniMetadonneesCLI"])
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-testing.git", from: "0.7.0")
+        .package(url: "https://github.com/apple/swift-testing.git", from: "0.7.0"),
+        .package(url: "https://github.com/Macthieu/OrchivisteKit.git", exact: "0.2.0")
     ],
     targets: [
         .target(name: "MuniMetadonneesCore"),
-        .executableTarget(name: "MuniMetadonneesCLI", dependencies: ["MuniMetadonneesCore"]),
+        .target(
+            name: "MuniMetadonneesInterop",
+            dependencies: [
+                "MuniMetadonneesCore",
+                .product(name: "OrchivisteKitContracts", package: "OrchivisteKit")
+            ]
+        ),
+        .executableTarget(
+            name: "MuniMetadonneesCLI",
+            dependencies: [
+                "MuniMetadonneesInterop",
+                .product(name: "OrchivisteKitContracts", package: "OrchivisteKit"),
+                .product(name: "OrchivisteKitInterop", package: "OrchivisteKit")
+            ]
+        ),
         .testTarget(
             name: "MuniMetadonneesTests",
             dependencies: [
                 "MuniMetadonneesCore",
+                "MuniMetadonneesInterop",
+                .product(name: "OrchivisteKitContracts", package: "OrchivisteKit"),
                 .product(name: "Testing", package: "swift-testing")
             ]
         )
